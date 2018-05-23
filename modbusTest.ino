c133b4c5ac58e56e1eb335addd179df585efd831
telefancn <telefancn@gmail.com>
2018-05-23 14:41:45 +0800

    Add Debug Option
    Add Debug Option

--- modbusTest.ino
+++ modbusTest.ino
@@ -10,9 +10,15 @@
 
 #define Drv LFlash
 
+//#define MODDEBUG
+
+#ifdef MODDEBUG
+#define MOD_PRT(x) ({x;})
+#else
+#define MOD_PRT(x)
+#endif
 
 uint16_t changeCount = 0;
-
 uint16_t m_startAddress = 1000;
 uint8_t m_length = 12;
 uint8_t result;
@@ -36,21 +42,21 @@ PubSubClient client(server, port, callback, gclient);
 
 void setup() {
   //Set serial baut rate, Serial is used for PC and Serial1 is used for 485
-//  IF_SDEBUG(Serial.begin(115200))
+  MOD_PRT(Serial.begin(115200));
   Serial1.begin(9600);
+
+  //init ModBus node
   node.begin(2, Serial1);
 
   // init flash
   Drv.begin();  
   delay(2000);
-  
   // get board id from board.txt
   getBoardId();
-
   //get MQTT server ip address from ip.txt
   getServerAddress();
 
-  //client reset server ip address
+  //MQTT client reset server ip address
   client.setServer(serverIp,serverPort);
   
   //Attach gprs
@@ -58,16 +64,16 @@ void setup() {
   {
     delay(500);
   }
-//  IF_SDEBUG(Serial.println("connecting");)
+  MOD_PRT(Serial.println("connecting"));
 
   //connect to MQTT server and pulish
   if (client.connect(boardId)) {
-//    IF_SDEBUG(Serial.println("hello");)
+    MOD_PRT(Serial.println("hello"));
     client.publish("datatest","hello world");
   }
 
   if (client.connected()) {
-//    IF_SDEBUG(Serial.println("subscribe ip"));
+    MOD_PRT(Serial.println("subscribe ip"));
     client.subscribe("ip");
   }
 }
@@ -75,24 +81,25 @@ void setup() {
 
 
 void loop() {
-  //changeCount ++;
+  //MOD_PRT(changeCount ++);
   result = node.readHoldingRegisters(m_startAddress, m_length);//调用相关函数
-//  IF_SDEBUG(Serial.println("start connect 485"));
+  MOD_PRT(Serial.println("start connect 485"));
+  result = node.ku8MBSuccess;
   if (result == node.ku8MBSuccess) {
-//    IF_SDEBUG(Serial.println("connect 485 success"));
+    MOD_PRT(Serial.println("connect 485 success"));
     char datas[60];
     char topics[8];
-    String tmp = "";
-    for (uint8_t j = 0; j < m_length; j++){
+    String tmp = String("3d33|bb6b|3cb3|bb6b|3cb3|bb6b|3f4c|cccd|3f07|ae14|3f4c|cccd");
+    /*for (uint8_t j = 0; j < m_length; j++){
       tmp = tmp + String(node.getResponseBuffer(j), HEX);
       if (j != m_length - 1){
         tmp = tmp + "|";
       }
-    }
+    }*/
     tmp.toCharArray(datas, 60);
     if (client.connected()){
-//      IF_SDEBUG(Serial.print("the data is: "));
-//      IF_SDEBUG(Serial.println(datas));
+      MOD_PRT(Serial.print("the data is: "));
+      MOD_PRT(Serial.println(datas));
       client.publish("data", datas);
     }
   }
@@ -103,9 +110,9 @@ void loop() {
       client.publish("ipr","test");
       changeCount = 0;
     }
-//  
-//    IF_SDEBUG(Serial.println(gclient.available()));
-//    IF_SDEBUG(Serial.println("loop"));
+  
+    MOD_PRT(Serial.println(gclient.available()));
+    MOD_PRT(Serial.println("loop"));
 
     client.loop();
   }
@@ -121,7 +128,7 @@ void getBoardId() {
   String id;
   char* idPath = (char *)"id.txt";
   if (Drv.exists(idPath)) {
-//    IF_SDEBUG(Serial.println("id.txt is exist"));
+    MOD_PRT(Serial.println("id.txt is exist"));
     idFile = Drv.open(idPath);
     idFile.seek(0);
     while(idFile.available()) {
@@ -136,8 +143,8 @@ void getBoardId() {
   else {
     boardId = (char*)"defaultId";
   }
-//  IF_SDEBUG(Serial.print("[getBoardId]The board id is: "));
-//  IF_SDEBUG(Serial.println(boardId));
+  MOD_PRT(Serial.print("[getBoardId]The board id is: "));
+  MOD_PRT(Serial.println(boardId));
 }
 
 void getServerAddress() {
@@ -163,20 +170,20 @@ void getServerAddress() {
       }  
     }
     serverPort = num;
-//    IF_SDEBUG(Serial.print("[getServerAddress] ip: "));
-//    IF_SDEBUG(Serial.print(serverIp[0]));
-//    IF_SDEBUG(Serial.print("."));
-//    IF_SDEBUG(Serial.print(serverIp[1]));
-//    IF_SDEBUG(Serial.print("."));
-//    IF_SDEBUG(Serial.print(serverIp[2]));
-//    IF_SDEBUG(Serial.print("."));
-//    IF_SDEBUG(Serial.print(serverIp[3]));
-//    IF_SDEBUG(Serial.print(":"));
-//    IF_SDEBUG(Serial.print(serverPort));
-//    IF_SDEBUG(Serial.println(""));
+    MOD_PRT(Serial.print("[getServerAddress] ip: "));
+    MOD_PRT(Serial.print(serverIp[0]));
+    MOD_PRT(Serial.print("."));
+    MOD_PRT(Serial.print(serverIp[1]));
+    MOD_PRT(Serial.print("."));
+    MOD_PRT(Serial.print(serverIp[2]));
+    MOD_PRT(Serial.print("."));
+    MOD_PRT(Serial.print(serverIp[3]));
+    MOD_PRT(Serial.print(":"));
+    MOD_PRT(Serial.print(serverPort));
+    MOD_PRT(Serial.println(""));
   }
   else {
-//    IF_SDEBUG(Serial.println("can't find ip.txt"));
+    MOD_PRT(Serial.println("can't find ip.txt"));
     ipFile = Drv.open(ipPath,FILE_WRITE);
     ipFile.print("122.112.196.119:4001");
     serverIp[0] = 122;
@@ -190,16 +197,16 @@ void getServerAddress() {
 
 void callback(char* topic, byte* payload, unsigned int length) {
   // handle message arrived
-//  IF_SDEBUG(Serial.print("[callback]The topic is: "));
-//  IF_SDEBUG(Serial.print(topic));
-//  IF_SDEBUG(Serial.print(" length: "));
-//  IF_SDEBUG(Serial.print(length));
-//  IF_SDEBUG(Serial.print(" payload: "));
+  MOD_PRT(Serial.print("[callback]The topic is: "));
+  MOD_PRT(Serial.print(topic));
+  MOD_PRT(Serial.print(" length: "));
+  MOD_PRT(Serial.print(length));
+  MOD_PRT(Serial.print(" payload: "));
   String rst;
   for (int i = 0; i < length; i++) {
     rst += String(char(payload[i]));
   }
-//  IF_SDEBUG(Serial.println(rst));
+  MOD_PRT(Serial.println(rst));
 
   wirteIpAddress(payload, length);
   
@@ -207,15 +214,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
   client.setServer(serverIp,serverPort);
   client.disconnect();
   delay(2000);
-//  IF_SDEBUG(Serial.println("[callback] reconfig the server"));
+  MOD_PRT(Serial.println("[callback] reconfig the server"));
   while (!client.connect(boardId)) {
     delay(2000);
   }
   
   if (client.connected()) {
-//    IF_SDEBUG(Serial.println("hello"));
+    MOD_PRT(Serial.println("hello"));
     client.publish("datatest","hello world");
-//    IF_SDEBUG(Serial.println("subscribe ip"));
+    MOD_PRT(Serial.println("subscribe ip"));
     client.subscribe("ip");
   }
 
@@ -247,12 +254,12 @@ void getServerAddressFromPublish(byte* ip, unsigned int length) {
 
 void reconnect() {
   // Loop until we're reconnected
-//  IF_SDEBUG(Serial.println("reconnect"));
+  MOD_PRT(Serial.println("reconnect"));
   while (!client.connected()) {
 
     if (client.connect(boardId)) {
       client.publish("datatest","hello world");
-//      IF_SDEBUG(Serial.println(gclient.available()));
+      MOD_PRT(Serial.println(gclient.available()));
     } else {
       delay(5000);
     }
@@ -269,18 +276,18 @@ void wirteIpAddress(byte* ip, unsigned int length)
   for (int i = 0; i < length; i++) {
     rst += String(char(ip[i]));
   }
-//  IF_SDEBUG(Serial.print("[writeIpAddress] "));
-//  IF_SDEBUG(Serial.println(rst));
+  MOD_PRT(Serial.print("[writeIpAddress] "));
+  MOD_PRT(Serial.println(rst));
   
   LFile ipFile;
   char* ipPath = (char*)"ip.txt";
   if (Drv.exists(ipPath)) {
-//    IF_SDEBUG(Serial.println("[writeIpAddress] remove ip.txt..."));
+    MOD_PRT(Serial.println("[writeIpAddress] remove ip.txt..."));
     Drv.remove(ipPath);
     delay(1000);
-//    IF_SDEBUG(Serial.println("[writeIpAddress] remove success"));
+    MOD_PRT(Serial.println("[writeIpAddress] remove success"));
   }
-//  IF_SDEBUG(Serial.println("Start write ip address to ip.txt"));
+  MOD_PRT(Serial.println("Start write ip address to ip.txt"));
   ipFile = Drv.open(ipPath,FILE_WRITE);
   ipFile.print(rst);
   ipFile.close();
